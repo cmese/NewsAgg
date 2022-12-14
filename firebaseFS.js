fd9ae390cb4f3d54aea7c72191437b0dd62c9ace
cmese <99047840+cmese@users.noreply.github.com>
2022-12-14 11:52:41 -0500

    added fs config to gitignore and removed cache, changed api key in gcp

--- firebaseFS.js
+++ firebaseFS.js
@@ -1,28 +1,12 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "firebase/firestore"
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA6OfZLlHiesH-JLEc5OjEnNiysjRaQg6c",
-//   authDomain: "newsagg-6ee94.firebaseapp.com",
-//   projectId: "newsagg-6ee94",
-//   storageBucket: "newsagg-6ee94.appspot.com",
-//   messagingSenderId: "895876007398",
-//   appId: "1:895876007398:web:546d753d4f909eed2a0227",
-//   measurementId: "G-82RNZKYY6F"
-// };
+// Your web app's Firebase configuration
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA6OfZLlHiesH-JLEc5OjEnNiysjRaQg6c",
-//   authDomain: "newsagg-6ee94.firebaseapp.com",
-//   projectId: "newsagg-6ee94",
-//   storageBucket: "newsagg-6ee94.appspot.com",
-//   messagingSenderId: "895876007398",
-//   appId: "1:895876007398:web:b4c6120d5bf49def2a0227",
-//   measurementId: "G-5GF5BR2D96"
-// };
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA00wuP2f5M85X1mHFkHgDwkp0qyfeYi68",
+  apiKey: "AIzaSyCifxtiKw-PUl_9rWaHKVFB_Fyup4nUud0",
   authDomain: "newsagg-6ee94.firebaseapp.com",
   projectId: "newsagg-6ee94",
   storageBucket: "newsagg-6ee94.appspot.com",
